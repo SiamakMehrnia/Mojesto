2a534f11220f4c80f3a250071033e8afc1756b6b
SiamakMehrnia <siamakme20@gmail.com>
2025-05-01 14:16:45 +0200

    fixrender

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => [
+    {
+      source: "/api/:path*",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
