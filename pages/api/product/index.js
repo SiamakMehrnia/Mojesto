5b4890960071f32bcda2c13a7fa56cb735bcb872
SiamakMehrnia <siamakme20@gmail.com>
2025-05-06 15:56:55 +0200

    fixProductPhoto

--- pages/api/product/index.js
+++ pages/api/product/index.js
@@ -1,26 +1,35 @@
-import { connectToDatabase } from '../../../utils/mongodb';
+import { connectToDatabase } from "../../../utils/mongodb";
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "5mb", // یا مثلاً '10mb' بسته به نیازت
+    },
+  },
+};
 
 export default async function handler(req, res) {
   try {
     const { db } = await connectToDatabase();
 
-    if (req.method === 'POST') {
+    if (req.method === "POST") {
       const { name, image } = req.body;
       if (!name || !image) {
         return res.status(400).json({ error: "Name and image are required" });
       }
-      const newProduct = await db.collection('product').insertOne({ name, image });
+      const newProduct = await db
+        .collection("product")
+        .insertOne({ name, image });
       return res.status(201).json(newProduct);
     }
 
-    if (req.method === 'GET') {
-      const products = await db.collection('product').find({}).toArray();
+    if (req.method === "GET") {
+      const products = await db.collection("product").find({}).toArray();
       return res.status(200).json(products);
     }
 
     res.status(405).end(`Method ${req.method} Not Allowed`);
   } catch (error) {
-    console.error('Error in /api/product:', error);
+    console.error("Error in /api/product:", error);
     res.status(500).json({ error: error.message });
   }
 }
